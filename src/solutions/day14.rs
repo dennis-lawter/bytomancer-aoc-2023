a142cbd8c5a0ecbfd08068017949f3365c6658b8
Bytomancer <dennis.lawter@gmail.com>
2023-12-14 00:44:30 -0500

    Day 14 cleaned up

--- src/solutions/day14.rs
+++ src/solutions/day14.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 
 use super::final_answer;
 use super::input_raw;
@@ -32,18 +31,6 @@ pub async fn d14s1(submit: bool, example: bool) {
     debug_char_map(&chars);
     let load = calc_north_load(&chars);
     final_answer(load, submit, DAY, 1).await;
-}
-
-fn are_equal(left: &Vec<Vec<char>>, right: &Vec<Vec<char>>) -> bool {
-    for y in 0..left.len() {
-        for x in 0..left[0].len() {
-            if left[y][x] != right[y][x] {
-                return false;
-            }
-        }
-    }
-
-    true
 }
 
 fn calc_north_load(chars: &Vec<Vec<char>>) -> usize {
@@ -168,16 +155,6 @@ fn roll_spin_cycle(chars: &mut Vec<Vec<char>>) {
     roll_east(chars);
 }
 
-fn deep_clone(map: &Vec<Vec<char>>) -> Vec<Vec<char>> {
-    let mut new = Vec::with_capacity(map.len());
-    for y in 0..map.len() {
-        let row = map[y].clone();
-        new.push(row);
-    }
-
-    new
-}
-
 fn hash_string(map: &Vec<Vec<char>>) -> String {
     let mut flat_vec: Vec<char> = Vec::with_capacity(map.len() * map[0].len());
     for y in 0..map.len() {
@@ -197,24 +174,21 @@ pub async fn d14s2(submit: bool, example: bool) {
 
     let mut hash_set: HashMap<String, usize> = HashMap::new();
     hash_set.insert(hash_string(&chars), 0);
-    // let mut previous_chars = deep_clone(&chars);
     debug_char_map(&chars);
     roll_spin_cycle(&mut chars);
-    debug_char_map(&chars);
+    // debug_char_map(&chars);
     let mut spins = 1;
     let mut traveling_hash = hash_string(&chars);
     while !hash_set.contains_key(&traveling_hash) {
-        // previous_chars = deep_clone(&chars);
         hash_set.insert(traveling_hash.clone(), spins);
         roll_spin_cycle(&mut chars);
-        debug_char_map(&chars);
+        // debug_char_map(&chars);
         traveling_hash = hash_string(&chars);
         spins += 1;
     }
     let first_spin_matched = hash_set.get(&traveling_hash).unwrap();
     println!("Spins {} vs {} match", first_spin_matched, spins);
     let period = spins - first_spin_matched;
-    // let period_advancement = spins;
     while spins + period < SPINS {
         spins += period;
     }
@@ -223,9 +197,7 @@ pub async fn d14s2(submit: bool, example: bool) {
         spins += 1;
     }
 
-    // for _i in 0..SPINS {
-    //     roll_spin_cycle(&mut chars);
-    // }
+    debug_char_map(&chars);
 
     let load = calc_north_load(&chars);
     final_answer(load, submit, DAY, 2).await;
