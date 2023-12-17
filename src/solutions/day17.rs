0d71962a86e92612c3db6fae8aba8982569e8ce2
Bytomancer <dennis.lawter@gmail.com>
2023-12-17 14:16:50 -0500

    Day 17 p1 solved, p2 in progress

--- src/solutions/day17.rs
+++ src/solutions/day17.rs
@@ -53,15 +53,21 @@ struct Traveler {
     dir: Direction,
     num_steps_in_same_dir: usize,
     heat_lost: usize,
+    // min_step_limit: usize,
+    max_step_limit: usize,
+    // history: Vec<Direction>,
 }
 impl Traveler {
-    fn new(x: i64, y: i64, dir: Direction) -> Self {
+    fn new(x: i64, y: i64, dir: Direction, max_step_limit: usize) -> Self {
         Self {
             x,
             y,
             dir,
             num_steps_in_same_dir: 0,
             heat_lost: 0,
+            // min_step_limit,
+            max_step_limit,
+            // history: vec![],
         }
     }
     fn can_travel(
@@ -70,12 +76,14 @@ impl Traveler {
         x_bounds: (usize, usize),
         y_bounds: (usize, usize),
     ) -> bool {
-        if self.num_steps_in_same_dir > 3 && self.dir == dir {
-            // can't take more than 3 steps in the same direction
+        if self.num_steps_in_same_dir >= self.max_step_limit && self.dir == dir {
             return false;
         }
+        // else if self.num_steps_in_same_dir < self.min_step_limit && self.dir != dir {
+        //     return false;
+        // }
         // prevent 180 degree turns
-        match dir {
+        match self.dir {
             Direction::North => {
                 if dir == Direction::South {
                     return false;
@@ -130,11 +138,12 @@ impl Traveler {
         //     && self.y >= y_bounds.0 as i64
         //     && self.y <= y_bounds.1 as i64
     }
-    fn travel(&mut self, dir: Direction) {
+    fn travel(&mut self, dir: Direction, heat_hits: &Vec<Vec<u8>>) -> bool {
+        // self.history.push(dir.clone());
         if self.dir == dir {
             self.num_steps_in_same_dir += 1;
         } else {
-            self.num_steps_in_same_dir = 0;
+            self.num_steps_in_same_dir = 1;
         }
         self.dir = dir;
         // print!("{}", self.dir);
@@ -145,80 +154,27 @@ impl Traveler {
             Direction::West => self.x -= 1,
             Direction::None => {}
         }
+        if self.y >= heat_hits.len() as i64
+            || self.x >= heat_hits[0].len() as i64
+            || self.y < 0
+            || self.x < 0
+        {
+            return false;
+        }
+        self.heat_lost += heat_hits[self.y as usize][self.x as usize] as usize;
+
+        true
     }
 }
-
-pub async fn d17s1(submit: bool, example: bool) {
-    let lines = input(example).await;
-    let heat_hits = lines_to_u8s(lines);
-    let x_bounds: (usize, usize) = (0, heat_hits[0].len() - 1);
-    let y_bounds: (usize, usize) = (0, heat_hits.len() - 1);
-    let starting_traveler = Traveler::new(0, 0, Direction::None);
-    let mut considering_paths = vec![starting_traveler];
-    let mut best_heat_loss = vec![vec![usize::MAX; heat_hits[0].len()]; heat_hits.len()];
-
-    let mut seen_travelers: HashSet<Traveler> = HashSet::new();
-
-    while !considering_paths.is_empty() {
-        if best_heat_loss[y_bounds.1 as usize][x_bounds.1 as usize] < usize::MAX {
-            break;
-        }
-        let mut traveler = pop_best_heat(&mut considering_paths);
-        traveler.heat_lost += heat_hits[traveler.y as usize][traveler.x as usize] as usize;
-        println!("{}", traveler.heat_lost);
-        // if traveler.y > y_bounds.1 as i64 || traveler.x > x_bounds.1 as i64 {
-        //     continue;
+impl std::fmt::Display for Traveler {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} heat, ", self.heat_lost)?;
+        // for h in self.history.iter() {
+        //     write!(f, "{}", h)?;
         // }
-        if traveler.heat_lost >= best_heat_loss[traveler.y as usize][traveler.x as usize] {
-            // continue;
-        } else {
-            best_heat_loss[traveler.y as usize][traveler.x as usize] = traveler.heat_lost;
-        }
-        if traveler.can_travel(Direction::North, x_bounds, y_bounds) {
-            let mut new_traveler = traveler.clone();
-            new_traveler.travel(Direction::North);
-            if !seen_travelers.contains(&new_traveler) {
-                considering_paths.push(new_traveler.clone());
-                seen_travelers.insert(new_traveler);
-            }
-            // println!("N");
-        }
-        if traveler.can_travel(Direction::East, x_bounds, y_bounds) {
-            let mut new_traveler = traveler.clone();
-            new_traveler.travel(Direction::East);
-            if !seen_travelers.contains(&new_traveler) {
-                considering_paths.push(new_traveler.clone());
-                seen_travelers.insert(new_traveler);
-            }
-            // println!("E");
-        }
-        if traveler.can_travel(Direction::South, x_bounds, y_bounds) {
-            let mut new_traveler = traveler.clone();
-            new_traveler.travel(Direction::South);
-            if !seen_travelers.contains(&new_traveler) {
-                considering_paths.push(new_traveler.clone());
-                seen_travelers.insert(new_traveler);
-            }
-            // println!("S");
-        }
-        if traveler.can_travel(Direction::West, x_bounds, y_bounds) {
-            let mut new_traveler = traveler.clone();
-            new_traveler.travel(Direction::West);
-            if !seen_travelers.contains(&new_traveler) {
-                considering_paths.push(new_traveler.clone());
-                seen_travelers.insert(new_traveler);
-            }
-            // println!("W");
-        }
-    }
 
-    final_answer(
-        best_heat_loss[y_bounds.1 as usize][x_bounds.1 as usize],
-        submit,
-        DAY,
-        1,
-    )
-    .await;
+        Ok(())
+    }
 }
 
 fn pop_best_heat(considering_paths: &mut Vec<Traveler>) -> Traveler {
@@ -236,7 +192,223 @@ fn pop_best_heat(considering_paths: &mut Vec<Traveler>) -> Traveler {
     trav
 }
 
+const IMPOSSIBLE_RETRACE: usize = 9;
+
+pub async fn d17s1(submit: bool, example: bool) {
+    let lines = input(example).await;
+    let heat_hits = lines_to_u8s(lines);
+    let x_bounds: (usize, usize) = (0, heat_hits[0].len() - 1);
+    let y_bounds: (usize, usize) = (0, heat_hits.len() - 1);
+    let starting_traveler = Traveler::new(0, 0, Direction::None, 3);
+    let mut considering_paths = vec![starting_traveler];
+    let mut best_heat_loss = vec![vec![usize::MAX; heat_hits[0].len()]; heat_hits.len()];
+
+    let mut seen_travelers: HashSet<Traveler> = HashSet::new();
+
+    // let mut debug_futility = 0;
+
+    while !considering_paths.is_empty() {
+        // if debug_futility > 10 {
+        //     break;
+        // } else {
+        //     debug_futility += 1;
+        // }
+        if best_heat_loss[y_bounds.1 as usize][x_bounds.1 as usize] < usize::MAX {
+            break;
+        }
+        let traveler = pop_best_heat(&mut considering_paths);
+        // println!("{}", traveler);
+        // if traveler.y > y_bounds.1 as i64 || traveler.x > x_bounds.1 as i64 {
+        //     continue;
+        // }
+
+        if traveler.heat_lost < best_heat_loss[traveler.y as usize][traveler.x as usize] {
+            best_heat_loss[traveler.y as usize][traveler.x as usize] = traveler.heat_lost;
+        }
+        if traveler.heat_lost
+            > best_heat_loss[traveler.y as usize][traveler.x as usize] + IMPOSSIBLE_RETRACE
+        {
+            continue;
+        }
+        if traveler.can_travel(Direction::North, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            if new_traveler.travel(Direction::North, &heat_hits) {
+                // new_traveler.heat_lost +=
+                //     heat_hits[new_traveler.y as usize][new_traveler.x as usize] as usize;
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("N");
+        }
+        if traveler.can_travel(Direction::East, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            if new_traveler.travel(Direction::East, &heat_hits) {
+                // new_traveler.heat_lost +=
+                //     heat_hits[new_traveler.y as usize][new_traveler.x as usize] as usize;
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("E");
+        }
+        if traveler.can_travel(Direction::South, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            if new_traveler.travel(Direction::South, &heat_hits) {
+                // new_traveler.heat_lost +=
+                //     heat_hits[new_traveler.y as usize][new_traveler.x as usize] as usize;
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("S");
+        }
+        if traveler.can_travel(Direction::West, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            if new_traveler.travel(Direction::West, &heat_hits) {
+                // new_traveler.heat_lost +=
+                //     heat_hits[new_traveler.y as usize][new_traveler.x as usize] as usize;
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("W");
+        }
+    }
+
+    final_answer(
+        best_heat_loss[y_bounds.1 as usize][x_bounds.1 as usize],
+        submit,
+        DAY,
+        1,
+    )
+    .await;
+}
+
 pub async fn d17s2(submit: bool, example: bool) {
-    let input = input(example).await;
-    final_answer(input[0].to_owned(), submit, DAY, 2).await;
+    let lines = input(example).await;
+    let heat_hits = lines_to_u8s(lines);
+    let x_bounds: (usize, usize) = (0, heat_hits[0].len() - 1);
+    let y_bounds: (usize, usize) = (0, heat_hits.len() - 1);
+    let starting_traveler = Traveler::new(0, 0, Direction::None, 10);
+    let mut considering_paths = vec![starting_traveler];
+    let mut best_heat_loss = vec![vec![usize::MAX; heat_hits[0].len()]; heat_hits.len()];
+
+    let mut seen_travelers: HashSet<Traveler> = HashSet::new();
+
+    // let mut debug_futility = 0;
+
+    while !considering_paths.is_empty() {
+        // if debug_futility > 10 {
+        //     break;
+        // } else {
+        //     debug_futility += 1;
+        // }
+        if best_heat_loss[y_bounds.1 as usize][x_bounds.1 as usize] < usize::MAX {
+            break;
+        }
+        let traveler = pop_best_heat(&mut considering_paths);
+        // println!("{}", traveler);
+        if traveler.y > y_bounds.1 as i64 || traveler.x > x_bounds.1 as i64 {
+            continue;
+        }
+
+        if traveler.heat_lost < best_heat_loss[traveler.y as usize][traveler.x as usize] {
+            best_heat_loss[traveler.y as usize][traveler.x as usize] = traveler.heat_lost;
+        }
+        if traveler.heat_lost
+            > best_heat_loss[traveler.y as usize][traveler.x as usize] + IMPOSSIBLE_RETRACE * 4
+        {
+            continue;
+        }
+        if traveler.can_travel(Direction::North, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            let success = if traveler.dir != Direction::North {
+                new_traveler.travel(Direction::North, &heat_hits)
+                    && new_traveler.travel(Direction::North, &heat_hits)
+                    && new_traveler.travel(Direction::North, &heat_hits)
+                    && new_traveler.travel(Direction::North, &heat_hits)
+            } else {
+                new_traveler.travel(Direction::North, &heat_hits)
+            };
+
+            if success {
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+
+            // println!("N");
+        }
+        if traveler.can_travel(Direction::East, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            let success = if traveler.dir != Direction::East {
+                new_traveler.travel(Direction::East, &heat_hits)
+                    && new_traveler.travel(Direction::East, &heat_hits)
+                    && new_traveler.travel(Direction::East, &heat_hits)
+                    && new_traveler.travel(Direction::East, &heat_hits)
+            } else {
+                new_traveler.travel(Direction::East, &heat_hits)
+            };
+
+            if success {
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("E");
+        }
+        if traveler.can_travel(Direction::South, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            let success = if traveler.dir != Direction::South {
+                new_traveler.travel(Direction::South, &heat_hits)
+                    && new_traveler.travel(Direction::South, &heat_hits)
+                    && new_traveler.travel(Direction::South, &heat_hits)
+                    && new_traveler.travel(Direction::South, &heat_hits)
+            } else {
+                new_traveler.travel(Direction::South, &heat_hits)
+            };
+
+            if success {
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("S");
+        }
+        if traveler.can_travel(Direction::West, x_bounds, y_bounds) {
+            let mut new_traveler = traveler.clone();
+            let success = if traveler.dir != Direction::West {
+                new_traveler.travel(Direction::West, &heat_hits)
+                    && new_traveler.travel(Direction::West, &heat_hits)
+                    && new_traveler.travel(Direction::West, &heat_hits)
+                    && new_traveler.travel(Direction::West, &heat_hits)
+            } else {
+                new_traveler.travel(Direction::West, &heat_hits)
+            };
+
+            if success {
+                if !seen_travelers.contains(&new_traveler) {
+                    considering_paths.push(new_traveler.clone());
+                    seen_travelers.insert(new_traveler);
+                }
+            }
+            // println!("W");
+        }
+    }
+
+    final_answer(
+        best_heat_loss[y_bounds.1 as usize][x_bounds.1 as usize],
+        submit,
+        DAY,
+        2,
+    )
+    .await;
 }
